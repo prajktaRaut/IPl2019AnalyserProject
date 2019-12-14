a36ed37d7e695150f488edeea8e1d529ab498e19
prajktaRaut <prajktasraut16@gmail.com>
2019-12-14 15:29:24 +0530

    Added test code to check for wrong file name should throw exception

--- src/test/java/com/iplanalysertest/IPLAnalyserTest.java
+++ src/test/java/com/iplanalysertest/IPLAnalyserTest.java
@@ -4,10 +4,12 @@ import com.bridgelabzs.CSVBuilderException;
 import com.iplanalyser.IPLAnalyser;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class IPLAnalyserTest {
 
     private final String IPL_FILE_PATH="/home/admin1/Documents/IPL2019Analyser/src/test/resources/IPL2019FactsheetMostRuns.csv";
+    private final String WRONG_IPL_FILE_PATH="./src/main/resources/IPL2019FactsheetMostRuns.csv";
     private final String INCORRECT_IPL_FILE="/home/admin1/Documents/IPL2019Analyser/src/test/resources/IPL2019Data.csv";
     private final String EMPTY_IPL_FILE="/home/admin1/Documents/IPL2019Analyser/src/test/resources/EmptyIPLFile.csv";
 
@@ -48,6 +50,17 @@ public class IPLAnalyserTest {
             result = iplAnalyser.loadIPLData(IPL_FILE_PATH);
             Assert.assertEquals(101,result);
         } catch (CSVBuilderException e) {
+        }
+    }
+
+    @Test
+    public void loadIPLData_WithWrongFile_ShouldThrowException() {
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CSVBuilderException.class);
+            iplAnalyser.loadIPLData(WRONG_IPL_FILE_PATH);
+        } catch (CSVBuilderException e) {
+            Assert.assertEquals(CSVBuilderException.ExceptionType.FILE_PROBLEM, e.type);
         }
     }
 }
