91a4d28b36d1f0c3bf53b782652dc2e8c983acd4
prajktaRaut <prajktasraut16@gmail.com>
2019-12-14 15:35:29 +0530

    Added test code to check for wrong file type should throw exception

--- src/test/java/com/iplanalysertest/IPLAnalyserTest.java
+++ src/test/java/com/iplanalysertest/IPLAnalyserTest.java
@@ -8,6 +8,7 @@ import org.junit.rules.ExpectedException;
 
 public class IPLAnalyserTest {
 
+    private static final String WRONG_IPL_FILE_TYPE = "/src/test/resources/IPL2019FactsheetMostRuns.txt";
     private final String IPL_FILE_PATH="/home/admin1/Documents/IPL2019Analyser/src/test/resources/IPL2019FactsheetMostRuns.csv";
     private final String WRONG_IPL_FILE_PATH="./src/main/resources/IPL2019FactsheetMostRuns.csv";
     private final String INCORRECT_IPL_FILE="/home/admin1/Documents/IPL2019Analyser/src/test/resources/IPL2019Data.csv";
@@ -59,6 +60,17 @@ public class IPLAnalyserTest {
             ExpectedException exceptionRule = ExpectedException.none();
             exceptionRule.expect(CSVBuilderException.class);
             iplAnalyser.loadIPLData(WRONG_IPL_FILE_PATH);
+        } catch (CSVBuilderException e) {
+            Assert.assertEquals(CSVBuilderException.ExceptionType.FILE_PROBLEM, e.type);
+        }
+    }
+
+    @Test
+    public void givenIndianStateCodeData_WithWrongFile_ShouldThrowException_() {
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CSVBuilderException.class);
+            iplAnalyser.loadIPLData(WRONG_IPL_FILE_TYPE);
         } catch (CSVBuilderException e) {
             Assert.assertEquals(CSVBuilderException.ExceptionType.FILE_PROBLEM, e.type);
         }
