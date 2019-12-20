bc8cf7231d023c3aa44d16e018b852a507804ba8
prajktaRaut <prajktasraut16@gmail.com>
2019-12-20 18:20:13 +0530

    Added test case to sort IplWicketsData based on Bowling Average

--- src/test/java/com/iplanalysertest/IPLAnalyserBowlingTest.java
+++ src/test/java/com/iplanalysertest/IPLAnalyserBowlingTest.java
@@ -96,4 +96,15 @@ public class IPLAnalyserBowlingTest {
             Assert.assertEquals(CSVBuilderException.ExceptionType.HEADER_OR_DELIMITER_PROBLEM, e.type);
         }
     }
+
+    @Test
+    public void sortIPLFileData_OnBowlingAverage_ShouldReturnSortedRecords() {
+        try {
+            iplAnalyser.loadIPLData(IPLAnalyser.IplDataType.WICKET,IPL_MOST_WKTS_FILE_PATH);
+            String iplCensusCSVS = iplAnalyser.sortIPLDataBasedOnFields(FieldNameForSorting.Average);
+            IPLWktsDataCSV[] CensusCSV = new Gson().fromJson(iplCensusCSVS, IPLWktsDataCSV[].class);
+            Assert.assertEquals("Krishnappa Gowtham", CensusCSV[0].player);
+        } catch (CSVBuilderException e) {
+        }
+    }
 }
