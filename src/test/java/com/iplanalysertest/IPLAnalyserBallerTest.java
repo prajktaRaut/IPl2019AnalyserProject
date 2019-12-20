f81577bc308bef6f142669d45e4637a3c489277f
prajktaRaut <prajktasraut16@gmail.com>
2019-12-20 15:21:25 +0530

    Added test to check if IPL Wickets Data file is empty should return true

--- src/test/java/com/iplanalysertest/IPLAnalyserBallerTest.java
+++ src/test/java/com/iplanalysertest/IPLAnalyserBallerTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 public class IPLAnalyserBallerTest {
 
+    private static final String EMPTY_IPL_FILE = "./src/test/resources/EmptyIPLFile.csv";
     IPLAnalyser iplAnalyser=new IPLAnalyser();
 
     private static final String INCORRECT_IPL_FILE = "./src/main/resources/IPL2019FactsheetMostWkts.csv";
@@ -22,6 +23,19 @@ public class IPLAnalyserBallerTest {
     public void method_ToCheck_IPLFileExist_ShouldReturnDoesNotExist() {
         String result = iplAnalyser.checkILLFilePresence(INCORRECT_IPL_FILE);
         Assert.assertEquals("DOES NOT EXIST", result);
+    }
+
+
+    @Test
+    public void method_ToCheck_EmptyIPLFile_ShouldReturnTrue() {
+        boolean result = iplAnalyser.checkIPLFileEmptyOrNot(EMPTY_IPL_FILE);
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void method_ToCheck_NOtEmptyIPLFile_ShouldReturnFalse() {
+        boolean result = iplAnalyser.checkIPLFileEmptyOrNot(IPL_MOST_WKTS_FILE_PATH);
+        Assert.assertEquals(false, result);
     }
 
     @Test
