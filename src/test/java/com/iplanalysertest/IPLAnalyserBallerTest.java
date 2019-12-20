699e0022aa3f8e393059eaaeec202174827c3add
prajktaRaut <prajktasraut16@gmail.com>
2019-12-20 15:16:01 +0530

    Added test to check if IPL Wickets Data file does not exist should Return NOT Exist

--- src/test/java/com/iplanalysertest/IPLAnalyserBallerTest.java
+++ src/test/java/com/iplanalysertest/IPLAnalyserBallerTest.java
@@ -8,12 +8,20 @@ import org.junit.Test;
 public class IPLAnalyserBallerTest {
 
     IPLAnalyser iplAnalyser=new IPLAnalyser();
+
+    private static final String INCORRECT_IPL_FILE = "./src/main/resources/IPL2019FactsheetMostWkts.csv";
     private final String IPL_MOST_WKTS_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostWkts.csv";
 
     @Test
     public void method_ToCheck_IPLFileExist_ShouldReturnExist() {
         String result = iplAnalyser.checkILLFilePresence(IPL_MOST_WKTS_FILE_PATH);
         Assert.assertEquals("EXIST", result);
+    }
+
+    @Test
+    public void method_ToCheck_IPLFileExist_ShouldReturnDoesNotExist() {
+        String result = iplAnalyser.checkILLFilePresence(INCORRECT_IPL_FILE);
+        Assert.assertEquals("DOES NOT EXIST", result);
     }
 
     @Test
